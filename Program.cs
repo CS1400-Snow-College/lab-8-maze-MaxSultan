4da59235f510021411b3a244caaa82b59d75844a
Max Sultan <maxsultankent@gmail.com>
2025-10-30 12:52:08 -0600

    Basic user controls

--- Program.cs
+++ Program.cs
@@ -1,7 +1,45 @@
 ﻿// Max Sultan, October 30th, Lab 8: Maze
+
+
+
+
+// (Load from file) Within your Main method, 
+// create a string array variable called mapRows and 
+// use File.ReadAllLines to load the contents of map.txt into your variable. 
+// Clear the screen, and then write a loop to print out the rows of the map to the screen. 
+// (Make sure the program works and commit the changes to your repo.)
+
+void DrawMap(){
+    string[] mapRows = File.ReadAllLines("./map.txt");
+    foreach(string row in mapRows)
+        Console.WriteLine(row);
+}
+
+void Main()
+{
+    Console.Title = "Maze Game";
+    Console.Clear();
 Console.Write(@"
 Welcome to Maze game.
 Avoid the #s
 Make it the the *
+Use Arrow keys to move
 Good Luck!
 ");
+    DrawMap();
+    Console.CursorLeft = 0;
+    Console.CursorTop = 6;
+    bool continuePlaying = true;
+    do {
+        ConsoleKey inputKey = Console.ReadKey(true).Key;
+        if(inputKey == ConsoleKey.Escape) {
+            continuePlaying = false;
+        }
+        if (inputKey == ConsoleKey.UpArrow) Console.CursorTop--;
+        else if (inputKey == ConsoleKey.DownArrow) Console.CursorTop++;
+        else if(inputKey == ConsoleKey.LeftArrow) Console.CursorLeft--;
+        else if (inputKey == ConsoleKey.RightArrow)	Console.CursorLeft++;
+    } while(continuePlaying);
+};
+
+Main();
--- obj/Debug/net8.0/lab-8-maze-MaxSultan.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-8-maze-MaxSultan.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-8-maze-MaxSultan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d91318ae94ceef568bfdbac2235b8c7c9715ce7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57b93f74013b14814400dced92864f41e657a1e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-8-maze-MaxSultan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-8-maze-MaxSultan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
